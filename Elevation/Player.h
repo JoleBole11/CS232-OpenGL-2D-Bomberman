c4659247b2375c3abae7b5b4a2dd0a22ba5518bb
JoleBole11 <bogdaniusc@gmail.com>
2025-09-25 16:00:11 +0200

    sprites fixed v2

--- Elevation/Player.h
+++ Elevation/Player.h
@@ -11,12 +11,16 @@ private:
     int* height_map;
     std::vector<std::vector<int>>* object_map;
     float bomb_cooldown = 0.0f;
+    bool dead = false;
 
     bool can_move_to_position(const glm::vec2& position);
 
 public:
     Player(glm::vec2 pos, glm::vec2 velocity, Sprite* sprite, int* _height_map, std::vector<std::vector<int>>* object_map);
     ~Player();
+
+    float get_speed() const { return speed; };
+	void set_speed(float s) { speed = s; };
 
     void update(float dt);
 };
